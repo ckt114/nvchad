96a2c59cd559c365098b4c6b9051ac3454e330b7
Chung Tran <chung.tran@liveramp.com>
2023-11-27 16:00:12 -0500

    dev

--- plugins/conform.lua
+++ plugins/conform.lua
@@ -2,7 +2,9 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      go = { "gofmt" },
       lua = { "stylua" },
+      markdown = { "mdformat" },
       python = { "isort", "black" },
       javascript = { { "prettierd", "prettier" } },
       terraform = { "terraform_fmt" },
